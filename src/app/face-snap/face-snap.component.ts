efe10908fc8444b0b262c39779f15c93d2ef34f1
FZbelgacha19 <fatimzahra.belgacha@gmail.com>
2022-04-07 21:35:53 +0000

    2

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaceSnapComponent implements OnInit {
 
-  constructor() { }
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
 
   ngOnInit(): void {
+    this.title = 'Archibald';
+    this.description = 'Mon meilleur ami depui toutes les années';
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.imageUrl = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60';
   }
-
 }
