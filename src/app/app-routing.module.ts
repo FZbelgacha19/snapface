887039f797f9fe95fb1627e04e952d79a0eb8647
FZbelgacha19 <fatimzahra.belgacha@gmail.com>
2022-04-11 15:52:15 +0000

    9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import {NewFaceSnapComponent} from "./new-face-snap/new-face-snap.component";
 
 const routes: Routes = [
     {path: '', component:LandingPageComponent},
     {path: 'facesnap', component: FaceSnapListComponent},
     {path: 'facesnap/:id', component: SingleFaceSnapComponent},
+    {path: "create", component: NewFaceSnapComponent}
 ]
 
 
